ba11cc42b36960aa3aba6193f41c0ff5e2cd38a3
Tony ElHabr <anthonyelhabr@gmail.com>
2019-11-18 06:10:30 -0600

    submit hw 6

--- project/99-eda.R
+++ project/99-eda.R
@@ -4,26 +4,59 @@ library(tidyverse)
 data <- 'data/epl.csv' %>% read_csv()
 data
 
-tms <- data %>% tetidy::pull_distinctly(tm_h)
-tms
-
-seasons <- data %>% tetidy::pull_distinctly(season)
-seasons
+# tms <- data %>% tetidy::pull_distinctly(tm_h)
+# tms
+# 
+# seasons <- data %>% tetidy::pull_distinctly(season)
+# seasons
 
 data_stack <-
   bind_rows(
-    data %>% 
+    data %>%
       mutate(tm = tm_h, g = g_h),
-    data %>% 
+    data %>%
       mutate(tm = tm_a, g = g_a)
   )
 data_stack
-data_stack %>% 
-  # group_by(tm) %>% 
-  count(tm, season, sort = T) %>% 
-  filter(n < 38)
+# data_stack %>% 
+#   # group_by(tm) %>% 
+#   count(tm, season, sort = T) %>% 
+#   filter(n < 38)
 
-fits_nested <-
+data_fct <-
+  data %>% 
+  mutate_at(vars(matches('^tm_')), as.factor)
+data_fct
+
+fit_g_partially <- purrr::partial(glm, data = data_fct, family = 'poisson', ... = )
+fit_g_h <- formula(g_h ~ tm_h + tm_a) %>% fit_g_partially()
+fit_g_a <- formula(g_a ~ tm_h + tm_a) %>% fit_g_partially()
+fit_g_h %>% broom::tidy()
+fit_g_a %>% broom::tidy()
+
+fit_g_diff <-
+  data_fct %>% 
+  glm(formula(g_diff ~ tm_h + tm_a), data = ., family = 'poisson')
+fit_g_diff
+
+fit_h <-
+  data_fct %>% 
+  glm(formula(g_h ~ tm_h), data = ., family = 'poisson')
+fit_h
+fit_h %>% 
+  broom::tidy() %>% 
+  mutate_at(vars(estimate), list(estimate_trans = ~exp(.))) %>% 
+  arrange(-estimate_trans)
+fit_h %>% 
+  broom::augment(type.predict = 'response') %>% 
+  filter(tm_h == 'Man City')
+
+fit_a <-
+  data_fct %>% 
+  glm(formula(g_a ~ tm_a), data = ., family = 'poisson')
+fit_a
+
+fits_by_tm_nested <-
   data_stack %>% 
   select(tm, g) %>% 
   group_by(tm) %>% 
@@ -31,13 +64,13 @@ fits_nested <-
   mutate(fit = purrr::map(data, ~glm(formula(g ~ 1), data = .x, family = 'poisson')))
 fits_nested
 
-terms_nested <-
+terms_by_tm_nested <-
   fits_nested %>% 
   select(-data) %>% 
   mutate(terms = purrr::map(fit, ~broom::tidy(.x))) %>% 
   select(-fit)
 
-terms_nested %>%
+terms_by_tm_nested %>%
   unnest(terms) %>% 
   arrange(desc(estimate)) %>% 
   mutate(est_trans = exp(estimate)) %>% 
@@ -47,13 +80,11 @@ terms_nested %>%
   geom_col() +
   coord_flip()
   
-
-summ_nested <-
+summ_by_tm_nested <-
   fits_nested %>% 
   select(-data) %>% 
   mutate(summ = purrr::map(fit, ~broom::glance(.x))) %>% 
   select(-fit)
-summ_nested
-summ_nested %>% 
-  unnest(summ)
+summ_by_tm_nested
+summ_by_tm_nested %>% unnest(summ)
   
